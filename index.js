04e747e33f6140cfcb0c1d09e6d67fc8819a2532
Girija-Sugumar <girija.sukumar@softsuave.org>
2024-02-19 16:50:40 +0530

    push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 4050
 
 app.get('/', (req, res) => {
-  res.send('pipeline testing')
+  res.send('testing testing')
 })
 app.get('/ecr', (req, res) => {
     res.send('new Image uploaded')
