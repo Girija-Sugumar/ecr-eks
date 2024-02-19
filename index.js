d1885faf7cd956f00baa3719193219ce6968aad9
Girija-Sugumar <girija.sukumar@softsuave.org>
2024-02-19 16:38:53 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express()
 const port = 4050
 
 app.get('/', (req, res) => {
-  res.send('Testing works')
+  res.send('pipeline testing')
 })
 app.get('/ecr', (req, res) => {
-    res.send('Image uploaded')
+    res.send('new Image uploaded')
 })
 app.get('/eks', (req, res) => {
-    res.send('Website deployed')
+    res.send('new changes deployed in Website')
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
